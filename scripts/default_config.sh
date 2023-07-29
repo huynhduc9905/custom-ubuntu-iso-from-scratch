1c397be981cb53450f9beabd72ac4d176c649c11
Huynh Huu Duc <94882243+huynhduc9905@users.noreply.github.com>
2023-07-29 16:57:00 +0700

    remove GUI pakage

--- scripts/default_config.sh
+++ scripts/default_config.sh
@@ -7,11 +7,11 @@
 
 # The version of Ubuntu to generate.  Successfully tested: bionic, cosmic, disco, eoan, focal, groovy, jammy
 # See https://wiki.ubuntu.com/DevelopmentCodeNames for details
-export TARGET_UBUNTU_VERSION="jammy"
+export TARGET_UBUNTU_VERSION="focal"
 
 # The Ubuntu Mirror URL. It's better to change for faster download.
 # More mirrors see: https://launchpad.net/ubuntu/+archivemirrors
-export TARGET_UBUNTU_MIRROR="http://us.archive.ubuntu.com/ubuntu/"
+export TARGET_UBUNTU_MIRROR="http://vn.archive.ubuntu.com/ubuntu/"
 
 # The packaged version of the Linux kernel to install on target image.
 # See https://wiki.ubuntu.com/Kernel/LTSEnablementStack for details
@@ -19,13 +19,13 @@ export TARGET_KERNEL_PACKAGE="linux-generic"
 
 # The file (no extension) of the ISO containing the generated disk image,
 # the volume id, and the hostname of the live environment are set from this name.
-export TARGET_NAME="ubuntu-from-scratch"
+export TARGET_NAME="mini"
 
 # The text label shown in GRUB for booting into the live environment
-export GRUB_LIVEBOOT_LABEL="Try Ubuntu FS without installing"
+export GRUB_LIVEBOOT_LABEL="Try Ubuntu without installing"
 
 # The text label shown in GRUB for starting installation
-export GRUB_INSTALL_LABEL="Install Ubuntu FS"
+export GRUB_INSTALL_LABEL="Install Ubuntu mini"
 
 # Packages to be removed from the target system after installation completes succesfully
 export TARGET_PACKAGE_REMOVE="
@@ -40,20 +40,16 @@ export TARGET_PACKAGE_REMOVE="
 # present on the installed system.
 function customize_image() {
     # install graphics and desktop
-    apt-get install -y \
-    plymouth-theme-ubuntu-logo \
-    ubuntu-gnome-desktop \
-    ubuntu-gnome-wallpapers
+    # apt-get install -y \
+    # plymouth-theme-ubuntu-logo \
+    # ubuntu-gnome-desktop \
+    # ubuntu-gnome-wallpapers
 
     # useful tools
     apt-get install -y \
-    clamav-daemon \
-    terminator \
-    apt-transport-https \
     curl \
     vim \
-    nano \
-    less
+    nano
 
     # purge
     apt-get purge -y \
